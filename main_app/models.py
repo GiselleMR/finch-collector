4bf5b6cef501b64dbef1028fe2cce968c21b9723
GiselleMR <giselle.rubin1@gmail.com>
2023-03-16 14:45:28 -0400

    debug create finch

--- main_app/models.py
+++ main_app/models.py
@@ -31,7 +31,7 @@ class Finch(models.Model):
         return f'{self.name} ({self.id})'
 
     def get_absolute_url(self):
-        return reverse('details', kwargs={'cat_id: self.id'})
+        return reverse('detail', kwargs={'finch_id': self.id})
 
 class Feeding(models.Model):
     date = models.DateField('Feeding Date')
